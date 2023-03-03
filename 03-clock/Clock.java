91035680e2b8639b89d18b74744072eb18dc2ede
Andy <ajscottlewis@gmail.com>
2023-03-03 12:55:24 +0000

    Add text colour formatting

--- 03-clock/Clock.java
+++ 03-clock/Clock.java
@@ -2,13 +2,15 @@ import java.time.LocalDateTime; // Import the LocalDateTime class
 import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
 
 class Clock {
-  public static void main(String[] args) {
+  public static final String ANSI_RED = "\u001B[31m";
+  public static final String ANSI_RESET = "\u001B[0m";
+  public static void main(String[] args) {    
     LocalDateTime dateTime = LocalDateTime.now();
     System.out.println("Before formatting: " + dateTime);
 
     DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
 
     String formattedTime = dateTime.format(dateTimeFormatter);
-    System.out.println("The local time is " + formattedTime);
+    System.out.println("After formatting: The local " + ANSI_RED + "time" + ANSI_RESET + " is " + formattedTime);
   }
 }
