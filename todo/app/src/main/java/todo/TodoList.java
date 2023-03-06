79dc57c3102c599c5b7c4045d78a85c1550bb905
Andy <ajscottlewis@gmail.com>
2023-03-06 16:46:29 +0000

    Test passes, todoList is empty at first

---
+++ todo/app/src/main/java/todo/TodoList.java
@@ -0,0 +1,14 @@
+package todo;
+
+import java.util.ArrayList;
+
+public class TodoList {
+  ArrayList<String> todos = new ArrayList<String>();
+  
+  public static void main(String[] args) {
+  }
+  
+  // void addTask(String task) {
+  //   todos.add(task);
+  // }
+}
---
+++ todo/app/src/test/java/todo/TodoListTest.java
@@ -0,0 +1,17 @@
+package todo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TodoListTest {
+  @Test public void shouldStartWithEmptyList() {
+    TodoList todoList = new TodoList();
+    assertTrue("it starts with an empty list", todoList.todos.isEmpty());
+  }
+  // @Test public void addsTaskToList() {
+  //   TodoList todoList = new TodoList();
+  //   todoList.addTask("Wash car");
+  //   assertFalse("it adds a todo to the list", todoList.todos.isEmpty());
+  // }
+}
