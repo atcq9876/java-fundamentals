390f945549d4de9de9bab63611f9e0484c8e828c
Andy <ajscottlewis@gmail.com>
2023-03-09 16:56:39 +0000

    Refactor: use .replace instead of append in loop

--- wordgame/app/src/main/java/wordgame/Game.java
+++ wordgame/app/src/main/java/wordgame/Game.java
@@ -7,17 +7,27 @@ public class Game {
   }
 
   public String getWordToGuess() {
-    StringBuilder hiddenWord = new StringBuilder();
-    for (int i = 0; i < wordToGuess.length(); i++) {
-      if (i == 0) {
-        hiddenWord.append(wordToGuess.charAt(0));
-      } else {
-        hiddenWord.append("_");
-      }
+    StringBuilder hiddenWordSB = new StringBuilder(wordToGuess);
+    for (int i = 1; i < wordToGuess.length(); i++) {
+      hiddenWordSB.replace(i, hiddenWordSB.length(), "_");
     }
-    return hiddenWord.toString();
+    return hiddenWordSB.toString();
   }
 
   public static void main(String[] args) {
   }
 }
+
+
+// ALTERNATIVE GETWORDTOGUESS METHOD:
+// public String getWordToGuess() {
+//   StringBuilder hiddenWordSB = new StringBuilder();
+//   for (int i = 0; i < wordToGuess.length(); i++) {
+//     if (i == 0) {
+//       hiddenWordSB.append(wordToGuess.charAt(0));
+//     } else {
+//       hiddenWordSB.append("_");
+//     }
+//   }
+//   return hiddenWordSB.toString();
+// }
