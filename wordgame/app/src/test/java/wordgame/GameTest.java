90ad6f3bb283fa93ce7f97e5ede1e237e87ae8a8
Andy <ajscottlewis@gmail.com>
2023-03-09 16:29:14 +0000

    Refactor: shorten assertEquals line

--- wordgame/app/src/test/java/wordgame/GameTest.java
+++ wordgame/app/src/test/java/wordgame/GameTest.java
@@ -6,6 +6,6 @@ import static org.junit.Assert.*;
 public class GameTest {
   @Test public void testGetsWordToGuess() {
     Game game = new Game("MAKERS");
-    assertEquals(game.getWordToGuess(), "MAKERS");
+    assertEquals(game.getWordToGuess(), "M_____");
   }
 }
