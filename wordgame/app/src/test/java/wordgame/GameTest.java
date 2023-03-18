64b849c0a77da5fa17ba51f9314510ed538b2232
Andy <ajscottlewis@gmail.com>
2023-03-18 12:33:50 +0000

    Add mocked Masker class to relevant tests

--- wordgame/app/src/test/java/wordgame/GameTest.java
+++ wordgame/app/src/test/java/wordgame/GameTest.java
@@ -8,28 +8,32 @@ import java.util.ArrayList;
 public class GameTest {
   @Test public void testGetsRemainingAttempts() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     assertEquals("Game begins with 10 attempts remaining", Integer.valueOf(10), game.getRemainingAttempts());
   }
 
   @Test public void testGetsRandomWord() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
     when(mockedWordGenerator.getRandomWordFromDictionary()).thenReturn("RANDOM");
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     assertEquals(game.wordToGuess, "RANDOM");
   }
 
   @Test public void testOneIncorrectGuessLetter() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
     when(mockedWordGenerator.getRandomWordFromDictionary()).thenReturn("LIGHT");
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     assertEquals(game.guessLetter('A'), false);
   }
   
   @Test public void testTwoIncorrectGuessLetters() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
     when(mockedWordGenerator.getRandomWordFromDictionary()).thenReturn("SHOWER");
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     game.guessLetter('A');
     assertEquals(game.guessLetter('B'), false);
   }
@@ -37,21 +41,24 @@ public class GameTest {
   @Test public void testOneCorrectGuessLetter() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
     when(mockedWordGenerator.getRandomWordFromDictionary()).thenReturn("BELT");
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     assertEquals(game.guessLetter('B'), true);
   }
 
   @Test public void testLowerCaseCorrectGuessLetter() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
     when(mockedWordGenerator.getRandomWordFromDictionary()).thenReturn("BELT");
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     assertEquals(game.guessLetter('e'), true);
   }
 
   @Test public void testIncorrectAndCorrectGuessLetters() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
     when(mockedWordGenerator.getRandomWordFromDictionary()).thenReturn("HOUSE");
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     game.guessLetter('C');
     assertEquals(game.guessLetter('U'), true);
   }
@@ -59,7 +66,8 @@ public class GameTest {
   @Test public void testGuessedLettersAreStored() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
     when(mockedWordGenerator.getRandomWordFromDictionary()).thenReturn("ALPHABET");
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     game.guessLetter('A');
     game.guessLetter('C');
     ArrayList<Character> expectedChars = new ArrayList<Character>();
@@ -71,7 +79,8 @@ public class GameTest {
   @Test public void testIsGameLostFalse() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
     when(mockedWordGenerator.getRandomWordFromDictionary()).thenReturn("TEST");
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     game.guessLetter('A');
     assertEquals(game.isGameLost(), false);
   }
@@ -79,7 +88,8 @@ public class GameTest {
   @Test public void testIsGameLostTrue() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
     when(mockedWordGenerator.getRandomWordFromDictionary()).thenReturn("TEST");
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     game.guessLetter('A');
     game.guessLetter('B');
     game.guessLetter('C');
@@ -96,7 +106,11 @@ public class GameTest {
   @Test public void testIsGameWonFalse() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
     when(mockedWordGenerator.getRandomWordFromDictionary()).thenReturn("BLAH");
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    ArrayList<Character> guessedLetters = new ArrayList<Character>();
+    guessedLetters.add('A');
+    when(mockedMasker.getHiddenWord("BLAH", guessedLetters)).thenReturn("B_A_");
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     game.guessLetter('A');
     assertEquals(game.isGameWon(), false);
   }
@@ -104,7 +118,14 @@ public class GameTest {
   @Test public void testIsGameWonTrue() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
     when(mockedWordGenerator.getRandomWordFromDictionary()).thenReturn("BLAH");
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    ArrayList<Character> guessedLetters = new ArrayList<Character>();
+    guessedLetters.add('B');
+    guessedLetters.add('L');
+    guessedLetters.add('A');
+    guessedLetters.add('H');
+    when(mockedMasker.getHiddenWord("BLAH", guessedLetters)).thenReturn("BLAH");
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     game.guessLetter('B');
     game.guessLetter('L');
     game.guessLetter('A');
@@ -115,10 +136,19 @@ public class GameTest {
   @Test public void testIsGameWonTrueRepeatedLetters() {
     WordGenerator mockedWordGenerator = mock(WordGenerator.class);
     when(mockedWordGenerator.getRandomWordFromDictionary()).thenReturn("LONDON");
-    Game game = new Game(mockedWordGenerator);
+    Masker mockedMasker = mock(Masker.class);
+    ArrayList<Character> guessedLetters = new ArrayList<Character>();
+    guessedLetters.add('L');
+    guessedLetters.add('O');
+    guessedLetters.add('N');
+    guessedLetters.add('E');
+    guessedLetters.add('D');
+    when(mockedMasker.getHiddenWord("LONDON", guessedLetters)).thenReturn("LONDON");
+    Game game = new Game(mockedWordGenerator, mockedMasker);
     game.guessLetter('L');
     game.guessLetter('O');
     game.guessLetter('N');
+    game.guessLetter('E');
     game.guessLetter('D');
     assertEquals(game.isGameWon(), true);
   }
